59676162b9bd1d6c7d8d4e1f3c91bc5663565a40
igedemahayana <mahayanaaa3@gmail.com>
2026-07-23 18:47:48 +0800

    Menambah Animasi GSAP untuk Frontend Section

--- src/js/script.js
+++ src/js/script.js
@@ -17,30 +17,62 @@ gsap.ticker.lagSmoothing(0);
 // GSAP → membuat elemen bergerak/animasi
 // ScrollTrigger → menjalankan animasi berdasarkan posisi scroll
 
+// Navbar
+gsap.from("#navbar", {
+  y: -50,
+  opacity: 0,
+  duration: 1,
+
+  scrollTrigger: {
+    trigger: "#navbar",
+    start: "bottom 80%",
+  },
+});
+
 // Hero Section
 
 gsap.from("#title-hero", {
   y: 50,
   opacity: 0,
   duration: 1,
+
+  scrollTrigger: {
+    trigger: "#title-hero",
+    start: "top 80%",
+  },
 });
 
 gsap.from("#paragraf-hero", {
   y: 30,
   opacity: 0,
   duration: 1,
+
+  scrollTrigger: {
+    trigger: "#paragraf-hero",
+    start: "top 80%",
+  },
 });
 
 gsap.from("#button-hero", {
-  y: 20,
+  y: 30,
   opacity: 0,
   duration: 1,
+
+  scrollTrigger: {
+    trigger: "#button-hero",
+    start: "top 80%",
+  },
 });
 
 gsap.from("#image-hero", {
   y: 100,
   opacity: 0,
   duration: 1,
+
+  scrollTrigger: {
+    trigger: "#image-hero",
+    start: "top 80%",
+  },
 });
 
 // Cards
@@ -77,6 +109,29 @@ gsap.from("#cards-3", {
 
   scrollTrigger: {
     trigger: "#cards-3",
+    start: "top 80%",
+  },
+});
+
+// Grid Tech Stack
+gsap.from("#section-frontend", {
+  y: 50,
+  opacity: 0,
+  duration: 1,
+
+  scrollTrigger: {
+    trigger: "#section-frontend",
+    start: "top 80%",
+  },
+});
+
+gsap.from("#grid-frontend-1", {
+  y: 50,
+  opacity: 0,
+  duration: 1,
+
+  scrollTrigger: {
+    trigger: "#grid-frontend-1",
     start: "top 80%",
   },
 });
